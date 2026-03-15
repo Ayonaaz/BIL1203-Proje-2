793910dae4f428bd7e510bed15449a4b4f76bd51
Ayona <naz.ekiz2406@gmail.com>
2026-03-15 11:55:06 +0300

    Update Search.c

--- Search.c
+++ Search.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h> 
-int main()
-{
-    char str[300]={"Selam guzeller ben dünyaaaa!\0"};
-    char bul[20]={"guzeller"};
-    char *sonuc = strstr(str,bul);
-    if(sonuc!=NULL)
-    {
-        printf("Bulunan kelime: %s\n",sonuc);
-    }
-    else
-    {
-        printf("Kelime bulunamadı.\n"); 
-    }
-    return 0;
+int main(){
+	char metin[10000]={"Yazılım dünyasında en sık karşılaşılan hatalardan biri, karakter dizilerinin sonuna 'null terminator' eklemeyi unutmaktır. C programlama dili bu konuda çok hassastır. Eğer dikkat edilmezse, bilgisayar hafızadaki rastgele karakterleri de ekrana basabilir."};
+	char bul[100]={"Karşılaştırılan\0"};
+	char *sonuc=strstr(metin,bul);
+	
+	if(sonuc!=NULL){
+		printf("%s aranan kelime\n",bul);
+	}
+	else{
+		printf("Kelime yok");
+	}
+	return 0;
+	
 }
