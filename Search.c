a10cece45c8b64ef6fd864ba0288e3ce2794df3e
Ayona <naz.ekiz2406@gmail.com>
2026-03-17 22:57:02 +0300

    Büyük Küçük Harf

--- Search.c
+++ Search.c
@@ -3,7 +3,7 @@
 
 int main(){
 	char metin[10000]={"My name is Alex. Every day, I wake up at 7:00 AM. I drink a cup of coffee and read the news. Then, I go to work by bus. I love my job because my colleagues are very friendly. In the evening, I cook dinner and watch a movie before I go to sleep."};
-	char bul[100]={"ALEX\0"};
+	char bul[100]={"MY"};
 	strlwr (metin);
 	strlwr (bul);
 	char *sonuc=strstr(metin,bul);
